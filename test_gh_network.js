817e535b90166fb4ca7d48ed25c9ddd68c9a5208
jtdaniel7 <jt@sdcadvisors.com>
2026-04-09 17:38:53 +0000

    Clean up Phase 2 dashboard files

--- test_gh_network.js
+++
@@ -1,6 +0,0 @@
-(async () => {
-  const res = await fetch('https://httpbin.org/get');
-  console.log('Status:', res.status);
-  const data = await res.json();
-  console.log('Origin IP:', data.origin);
-})();
